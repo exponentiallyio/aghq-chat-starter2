e9057097fd56e616708fa0a6cd497c4c8b8de009
exponentiallyio <127355034+exponentiallyio@users.noreply.github.com>
2023-03-23 14:17:08 +0000

    Text-only to toggle switch

--- src/App.vue
+++ src/App.vue
@@ -318,12 +318,12 @@ export default {
         <i class="icon undo"></i>
         Start over
       </button>
-      <button
-        @click="textOnly = !textOnly"
-        class="ui toggle right floated button"
-      >
-        Text Only
-      </button>
+      <!-- Rounded switch -->
+      <label class="switch">
+        <input type="checkbox" v-model="textOnly">
+        <span class="slider round"></span>
+      </label>
+
 
     </div>
   </div>
@@ -333,6 +333,70 @@ export default {
 p {
   line-height: 3em;
 }
+
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
 
 .loader {
   width: 12px;
