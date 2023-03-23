beab65da08940003385e3d959fcea4dc46f50789
exponentiallyio <127355034+exponentiallyio@users.noreply.github.com>
2023-03-23 11:21:27 +0000

    Grey background

--- src/App.vue
+++ src/App.vue
@@ -121,29 +121,6 @@ export default {
       $this.recognition.start();
     },
 
-
-    /* BEFORE continuous listening
-    listen() {
-      if (this.listening) {
-        this.stopListening();
-        return;
-      }
-      const $this = this;
-      $this.listening = true;
-      $this.recognition = new speechRecognition();
-      $this.recognition.lang = $this.browserLanguage;
-
-      $this.recognition.start();
-      $this.recognition.onresult = function (event) {
-        const transcript = event.results[0][0].transcript;
-        $this.prompt = transcript;
-        $this.submitForm(transcript, true);
-        $this.listening = false;
-      };
-    },
-
-    */
-
     scrollToBottom() {
       this.$nextTick(() => {
         var promptElements = document.querySelectorAll(".prompt");
@@ -192,18 +169,6 @@ export default {
           });
 
 
-          /*
-          this.showTextSlowly(result).then(() => {
-            
-            
-            this.prompts.push({
-              prompt: this.prompt,
-              response: result,
-            });
-          });
-          */
-
-
           /* Show text all at once
           this.prompts.push({
             prompt: this.prompt,
@@ -233,7 +198,7 @@ export default {
 </script>
 
 <template>
-  <div class="ui basic segment">
+  <div class="ui basic segment" style="background-color: #272727;">
     <div
       v-show="listening"
       class="ui active blurring page inverted content dimmer"
@@ -253,14 +218,14 @@ export default {
         z-index: 9999;
         padding-top: 2em;
         padding-bottom: 2em;
-        background: #fff;
+        background: ##272727;
       "
     >
-      <h1 class="ui center aligned page header">
+      <h1 class="ui center aligned page header" style="color: white;">
         <span class="text">{{ appName }}</span>
       </h1>
   
-      <h3 class="ui center aligned header" style="margin-top: 1em; margin-bottom: 2em;">““The biggest mistake most people make is to not see themselves and others objectively.” – Ray Dalio</h3> <!-- Add margin to the subtitle -->
+      <h3 class="ui center aligned header" style="margin-top: 1em; margin-bottom: 2em; color: white;">““The biggest mistake most people make is to not see themselves and others objectively.” – Ray Dalio</h3> <!-- Add margin to the subtitle -->
 
       
       <form
@@ -288,30 +253,6 @@ export default {
         </div>
       </form>
 
-      <!-- OLD NON-resizable single line input
-      <form
-        ref="form"
-        @submit.prevent="submitForm"
-        :class="{ loading }"
-        class="ui huge form bottom"
-      >
-        <div class="field">
-          <div class="ui right action left icon input">
-            <i class="microphone icon link" @click="listen"></i>
-            <input
-              type="text"
-              ref="input"
-              @keydown.esc.stop.prevent="stopListening"
-              @keydown.shift.space.exact.stop.prevent="listen"
-              @keydown.enter.exact.stop.prevent="submitForm"
-              @focus="focused = true"
-              @blur="focused = false"
-              v-model="prompt"
-            />
-            <button type="submit" class="ui button primary">Go!</button>
-          </div>
-        </div>
-      </form> -->
     </div>
 
     <div style="margin-top: 2em">
@@ -323,7 +264,7 @@ export default {
       <template v-for="(prompt, index) in prompts">
         <tr class="prompt">
           <td class="collapsing">
-            <em data-emoji=":thinking:" class="medium"></em>
+            <em data-emoji=":speech_balloon:" class="medium"></em>
           </td>
           <td>
             <p>
@@ -354,8 +295,8 @@ export default {
       <button
         v-if="prompts.length > 0"
         @click="clearPrompts"
-        class="ui tertiary icon button"
-      >
+        class="ui tertiary icon button start-over-button"
+        >
         <i class="icon undo"></i>
         Start over
       </button>
@@ -366,6 +307,14 @@ export default {
 <style scoped>
 p {
   line-height: 3em;
+}
+
+.start-over-button {
+  color: lightgrey; /* Set the default color to light grey */
+}
+
+.start-over-button:hover {
+  color: white !important; /* Set the color to white on hover */
 }
 
 a.hovering {
