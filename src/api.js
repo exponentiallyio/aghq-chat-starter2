cf8ed77bb744423e28dc9bfd13eb04af7bd7b882
exponentiallyio <127355034+exponentiallyio@users.noreply.github.com>
2023-03-26 17:40:44 +0100

    Remove Azure, back to Google Cloud

--- src/api.js
+++ src/api.js
@@ -1,53 +1,19 @@
 import fetch from "node-fetch";
 import express from "express";
-
 // Removed
 // const elevenLabsApiBaseUrl = import.meta.env.VITE_ELEVENLABS_API_BASE_URL;
 // const elevenLabsApiKey = import.meta.env.VITE_ELEVENLABS_API_KEY;
 // const elevenLabsVoiceId = import.meta.env.VITE_ELEVENLABS_VOICE_ID;
 
-// Remove VITE_ prefix
-const googleCloudLanguageCode = process.env.GOOGLE_CLOUD_LANGUAGE_CODE;
-const googleCloudVoiceID = process.env.GOOGLE_CLOUD_VOICE_ID;
-const googleCloudApiKey = process.env.GOOGLE_CLOUD_API_KEY;
+// Google Cloud TTS variables
+const googleCloudLanguageCode = import.meta.env.VITE_GOOGLE_CLOUD_LANGUAGE_CODE;
+const googleCloudVoiceID = import.meta.env.VITE_GOOGLE_CLOUD_VOICE_ID;
+const googleCloudApiKey = import.meta.env.VITE_GOOGLE_CLOUD_API_KEY;
 
-const azureKey = process.env.AZURE_SPEECH_KEY;
-const azureRegion = process.env.AZURE_SPEECH_REGION;
-
-// For Azure STT
-import fs from "fs";
-import * as sdk from "microsoft-cognitiveservices-speech-sdk";
-
-const speechConfig = sdk.SpeechConfig.fromSubscription(azureKey, azureRegion);
-speechConfig.speechRecognitionLanguage = "en-US";
-
-// function to handle audio data received from the client-side
-async function transcribeAudio(audioBuffer) {
-  const speechConfig = sdk.SpeechConfig.fromSubscription(azureKey, azureRegion);
-  speechConfig.speechRecognitionLanguage = "en-US";
-
-  const audioConfig = sdk.AudioConfig.fromStreamInput(new MemoryStream(audioBuffer));
-  const recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
-
-  return new Promise((resolve, reject) => {
-    recognizer.recognizeOnceAsync(
-      (result) => {
-        recognizer.close();
-
-        if (result.reason === sdk.ResultReason.RecognizedSpeech) {
-          resolve(result.text);
-        } else {
-          reject(new Error("Failed to recognize speech"));
-        }
-      },
-      (error) => {
-        recognizer.close();
-        reject(error);
-      }
-    );
-  });
-}
-
+// Retune Chatbot API variables
+const retuneApiKey = import.meta.env.VITE_RETUNE_API_KEY;
+const retuneThreadId = import.meta.env.VITE_RETUNE_THREAD_ID;
+const retuneApiUrl = import.meta.env.VITE_RETUNE_API_CONTINUE_URL;
 
 // Updated function to use Google Cloud Text-to-speech
 async function textToSpeech(text) {
@@ -66,7 +32,7 @@ async function textToSpeech(text) {
       },
       audioConfig: {
         audioEncoding: "LINEAR16",
-        pitch: 6.0, // Adjust the pitch
+        pitch: 20.0, // Adjust the pitch
         speakingRate: 1.0, // Adjust the speaking rate
       },
     }),
@@ -85,20 +51,58 @@ async function textToSpeech(text) {
 }
 
 
-
+/* Using Agent-HQ endpoint
 let endpoint = "https://app.agent-hq.io";
 if (import.meta.env.VITE_AGHQ_ENDPOINT) {
   endpoint = import.meta.env.VITE_AGHQ_ENDPOINT;
 }
+*/
 
 const api_access_token = import.meta.env.VITE_AGHQ_API_ACCESS_TOKEN;
 
 const agent_id = import.meta.env.VITE_AGHQ_AGENT_ID;
 
+// RETUNE FUNCTIONS
+// Function to create a new thread with Retune
+async function getRetuneResponse(input, threadId = null) {
+  const requestBody = {
+    input,
+  };
+
+  let endpoint = `${retuneApiBaseUrl}/api/chat/${retuneModelId}/`;
+
+  if (threadId) {
+    endpoint += "response";
+    requestBody.threadId = threadId;
+  } else {
+    endpoint += "new-thread";
+  }
+
+  const response = await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Retune-API-Key": retuneApiKey,
+    },
+    body: JSON.stringify(requestBody),
+  });
+
+  if (!response.ok) {
+    console.error('Error with Retune request:', await response.text());
+    throw new Error(`Retune API call failed: ${response.statusText}`);
+  }
+
+  return await response.json();
+}
+
+
+// Express app setup
+
 const app = express();
 app.use(express.json());
 
 
+/*
 app.post("/api", async (req, res) => {
   try {
     const response = await fetch(`${endpoint}/api/v1/agents/${agent_id}/run`, {
@@ -132,52 +136,41 @@ app.post("/api", async (req, res) => {
     res.status(500).send('Unexpected error');
   }
 });
+*/
 
-// Add the new /api/transcribe route for Azure below your existing /api route
-app.post("/api/transcribe", async (req, res) => {
+// Main API route using Retune API for chatbot
+app.post("/api", async (req, res) => {
   try {
-    const { audioData } = req.body;
-    const buffer = Buffer.from(audioData);
-    console.log("Audio data received:", audioData);
-
-    // Set up the Azure Speech Service client
-    const speechConfig = sdk.SpeechConfig.fromSubscription(azureKey, azureRegion);
-    speechConfig.speechRecognitionLanguage = "en-US";
-
-    // Create an audio stream from the buffer
-    const pushStream = sdk.AudioInputStream.createPushStream();
-    pushStream.write(buffer);
-    pushStream.close();
-
-    // Set up the speech recognizer
-    const audioConfig = sdk.AudioConfig.fromStreamInput(pushStream);
-    const recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
-
-
-
-    // Start the recognition and return the result
-    recognizer.recognizeOnceAsync(
-      (result) => {
-        recognizer.close();
-        const transcription = result.text || "No transcription found";
-        console.log("Server-side transcription:", transcription);
-        console.log("Recognition result object:", result);
-        res.json({ transcription: transcription });
+    // Replace Agent-HQ API call with Retune API call
+    const response = await fetch(retuneApiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Retune-API-Key": retuneApiKey,
       },
-      (err) => {
-        recognizer.close();
-        console.error("Error in transcription:", err);
-        res.status(500).send("Error transcribing audio");
-      }
-    );
+      body: JSON.stringify({
+        threadId: retuneThreadId,
+        input: req.body.input,
+      }),
+    });
 
+    if (!response.ok) {
+      console.error('Error with Retune request:', await response.text());
+      return res.status(response.status).send('Error with Retune request');
+    }
+
+    const data = await response.json();
+    const textResponse = data.response.value;
+
+    const audioBuffer = await textToSpeech(textResponse);
+    const audioData = audioBuffer.toString("base64");
+
+    res.status(200).json({ text: textResponse, audioData });
   } catch (error) {
-    console.error("Error in /api/transcribe:", error);
-    res.status(500).send("Error processing request");
+    console.error('Unexpected error:', error);
+    res.status(500).send('Unexpected error');
   }
 });
-
-
 
 
 
